ccbca4732f2c093ee9b5a2cf38cae0040d3c69bc
Razboy20 <razboy20@gmail.com>
2022-01-19 09:59:35 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ sleep 1
 cd /home/container
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`exec echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=`echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+exec env ${MODIFIED_STARTUP}
